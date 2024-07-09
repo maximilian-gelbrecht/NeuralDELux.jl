f136a6549198bb0c9c47db9197b3bcf10ef0dd0b
Max <maximilian.gelbrecht@posteo.de>
2024-07-09 20:07:11 +0200

    remake_loss instead of remake to avoid function ambigouities

--- src/training.jl
+++ src/training.jl
@@ -5,7 +5,7 @@ using NODEData, Optimisers, Zygote, StatsBase, Random, Printf, JLD2, Adapt
 """
     model, ps, st, training_results = train!(model, ps, st, loss, train_data, opt_state, η_schedule; τ_range=2:2, N_epochs=1, verbose=true, save_name=nothing, save_results_name=nothing, shuffle_data_order=true, additional_metric=nothing, valid_data=nothing, test_data=nothing, scheduler_offset::Int=0, compute_initial_error::Bool=true, save_mode::Symbol=:valid, adjust_loss_func::Bool = false)
 
-Trains the `model` with parameters `ps` and state `st` with the `loss` function and `train_data` by applying a `opt_state` with the learning rate `η_schedule` for `N_epochs`. Returns the trained `model`, `ps`, `st`, `results`. An `additional_metric` with the signature `(model, ps, st) -> value` might be specified that is computed after every epoch. `save_mode` determines if the model is saved with the lowest error on the `:valid` set or `:train` set. If `adjust_loss_func==true` for each new forecast length `loss = remake(loss, length)` is called. 
+Trains the `model` with parameters `ps` and state `st` with the `loss` function and `train_data` by applying a `opt_state` with the learning rate `η_schedule` for `N_epochs`. Returns the trained `model`, `ps`, `st`, `results`. An `additional_metric` with the signature `(model, ps, st) -> value` might be specified that is computed after every epoch. `save_mode` determines if the model is saved with the lowest error on the `:valid` set or `:train` set. If `adjust_loss_func==true` for each new forecast length `loss = remake_loss(loss, length)` is called. 
 """
 function train!(model, ps, st, loss, train_data, opt_state, η_schedule; τ_range=2:2, N_epochs=1, verbose=true, save_name=nothing, save_results_name=nothing, shuffle_data_order=true, additional_metric=nothing, valid_data=nothing, test_data=nothing, scheduler_offset::Int=0, compute_initial_error::Bool=true, save_mode::Symbol=:valid, adjust_loss_func::Bool=false)
 
@@ -24,7 +24,7 @@ function train!(model, ps, st, loss, train_data, opt_state, η_schedule; τ_rang
             train_data = NODEDataloader(train_data, τ) 
         end 
 
-        loss = adjust_loss_func ? remake(loss, τ) : loss 
+        loss = adjust_loss_func ? remake_loss(loss, τ) : loss 
 
         # initial error 
         lowest_train_err = compute_initial_error ? mean([loss(train_data[i], model, ps, st)[1] for i=1:length(train_data)]) : Inf
